c0781ba8d6c1c330b7e635d3bd9281ff22b6dd5c
ldsvrn <louis2555@orange.fr>
2022-01-05 11:27:41 +0100

    find 1 et 2

---
+++ find1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+def main(argv):
+    if not argv:
+        print("ERREUR: Pas assez d'arguments!")
+        quit()
+
+    for folder in argv:
+        if not os.path.exists(folder):
+            print(f"ERREUR: Le dossier {folder} n'existe pas!")
+            continue
+
+        print(f"\n\t{folder}:")
+        for files in os.listdir(folder):
+            print(files)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ find2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+def main(argv):
+    if not argv:
+        print("ERREUR: Pas assez d'arguments!")
+        quit()
+
+    for dir in argv:
+        if not os.path.exists(dir):
+            print(f"ERREUR: Le dossier {dir} n'existe pas!")
+            continue
+
+        # os.chdir(dir)
+        files = []
+        dirs = []
+        for obj in os.listdir(dir):
+            if os.path.isdir(dir + "/" + obj):
+                dirs.append(obj)
+            elif os.path.isfile(dir + "/" + obj):
+                files.append(obj)
+
+    print("\t---- DOSSIERS ----")
+    for i in dirs:
+        print(i, end=" ")
+
+    print("\n\t---- FICHIERS ----")
+    for i in files:
+        print(i, end=" ")
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
