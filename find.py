fc28309351878fbf4a5357c6399847566ebe92ec
ldsvrn <louis2555@orange.fr>
2022-01-05 12:03:01 +0100

    vrai fin de l'heure

--- find.py
+++ find.py
@@ -5,6 +5,16 @@ import os
 import getopt
 
 
+def aide(msg):
+    print(msg)
+    print(...)  # affiche le nom du script et comment il faut appeler ce script
+    exit()
+
+
+    def recherche(dossier, fichier):
+        contenuDuRépertoire = os.listdir(...)
+
+
 def main(argv):
     if not argv:
         print("ERREUR: Pas assez d'arguments!")
@@ -17,6 +27,31 @@ def main(argv):
         quit()
 
     print(opts, args)
+
+    rootdir = ""
+    toSearch = ""
+    for i in opts:
+        match i[0]:
+            case '-d':
+                rootdir = i[1]
+            case '-f':
+                toSearch = i[1]
+
+    if not os.path.exists(rootdir):
+        print(f"ERREUR: {rootdir} n'existe pas!")
+        quit(2)
+    elif not os.path.isdir(rootdir):
+        print(f"ERREUR: {rootdir} n'est pas un dossier!")
+        quit(20)
+
+    dirs = []
+    files = []
+
+    for item in rootdir:  # pour chaque élément (elt) du répertoire
+        if os.path.isdir(item):  # si c’est un dossier
+            dirs.append(os.path.join(rootdir, item))
+        elif os.path.isfile(item):  # sinon si c’est un fichier
+            files.append(os.path.join(rootdir, item))
 
 
 if __name__ == '__main__':
